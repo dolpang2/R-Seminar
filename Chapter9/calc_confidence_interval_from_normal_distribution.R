a37e4b727b7c691662a2b265c5bcebda63d13eb0
Geunho Lee <lkh7689@gmail.com>
2015-04-03 00:22:33 +0900

    Revise constant and variable

--- Chapter9/calc_confidence_interval_from_normal_distribution.R
+++ Chapter9/calc_confidence_interval_from_normal_distribution.R
@@ -1,7 +1,12 @@
-sample.mean <- 5
-sample.standard.deviation <- 2
-sample.size <- 20
-error <- qnorm(0.975) * sample.standard.deviation / sqrt(sample.size)
+kConfidenceCoefficient <- 0.95
+kAlpah <- 1 - kConfidenceCoefficient
+
+sample.mean <- 5  # sample's mean
+sample.standard.deviation <- 2  # s(Sample's standard deviation)
+sample.size <- 20  # size of sample
+
+error <- qnorm(kConfidenceCoefficient + kAlpah / 2) * 
+  sample.standard.deviation / sqrt(sample.size)
 left <- sample.mean - error
 right <- sample.mean + error
 left

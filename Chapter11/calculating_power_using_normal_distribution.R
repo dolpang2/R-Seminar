d14bd03e1b6447abea17cdd5717c6b20942d5db5
Geunho Lee <lkh7689@gmail.com>
2015-04-30 23:01:55 +0900

    Save

---
+++ Chapter11/calculating_power_using_normal_distribution.R
@@ -0,0 +1,24 @@
+kConfidenceCoefficient <- 0.95
+kAlpha <- 1 - kConfidenceCoefficient
+
+sample.mean <- 5
+sample.standard.deviation <- 2
+sample.size <- 20
+
+error <- qnorm(kConfidenceCoefficient + kAlpha / 2) * 
+  sample.standard.deviation / sqrt(sample.size)
+
+left <- sample.mean - error
+right <- sample.mean + error
+left
+right
+
+assumed <- sample.mean + 1.5
+gaussian.left <- (left - assumed) / (sample.standard.deviation / sqrt(sample.size))
+gaussian.right <- (right - assumed) / (sample.standard.deviation / sqrt(sample.size))
+
+beta <- pnorm(gaussian.right) - pnorm(gaussian.left)
+beta
+
+statistical.power <- 1 - beta
+statistical.power

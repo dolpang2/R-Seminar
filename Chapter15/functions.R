e5f9235784a93bc6a0c2ab85a075a5e040369ef4
Geunho Lee <lkh7689@gmail.com>
2015-05-11 16:37:31 +0900

    Explain function code

---
+++ Chapter15/functions.R
@@ -0,0 +1,11 @@
+newDef <- function(a, b) {
+  x <- runif(10, a, b)
+  mean(x)
+}
+
+c <- c(1, 2, 3, 4, 5)
+sample <- function(a, b) {
+  value <- switch(a, "median" = median(b), "mean" = mean(b), "variance" = var(b))
+  largeVals <- length(c[c > 1])
+  list(stat = value, number = largeVals)
+}
